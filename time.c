5d8636bc9280cfaf7aa053f42730a09cffa83727
Thomas Herbin <thomas@herbin.xyz>
2022-10-09 18:09:30 +0200

    offsset for graph

--- time.c
+++ time.c
@@ -1,9 +1,9 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
 #include "pbPlots.h"
 #include "supportLib.h"
 #include "time.h"
-#include <sys/time.h>
-#include <unistd.h>
 
 int draw_image(double *xs, double *ys, int size)
 {
@@ -39,15 +39,18 @@ int main()
     struct timeval *tv_diff;
     struct timeval *tv_started;
     unsigned long timemillisec;
-    int arraysize = 512;
+    const int arraysize = 512;
     // double xs[] = {-2, -1, 0, 1, 2};
     // double ys[] = {2, -1, -2, -1, 2};
-    double xs[arraysize];
-    double ys[arraysize];
+    double *xs;
+    double *ys;
+
     int timeoffset;
 
     tv_diff = malloc(sizeof(*tv_diff));
     tv_started = malloc(sizeof(*tv_started));
+    xs = malloc(sizeof(double) * arraysize);
+    ys = malloc(sizeof(double) * arraysize);
 
     gettimeofday(tv_started, NULL);
 
@@ -56,15 +59,15 @@ int main()
     for (int i = 0; i < 256; i++)
     {
         gettimeofday(tv_diff, NULL);
-        // Get the real starting value so that the graph starts at 0
-        if (i == 0)
-            timeoffset = tv_diff->tv_sec;
 
         timemillisec = ((tv_diff->tv_sec - tv_started->tv_sec) * 1000) + (tv_diff->tv_usec / 1000);
-        //timemillisec -= timeoffset;
+        // Get the real starting value so that the graph starts at 0
+        if (i == 0)
+            timeoffset = timemillisec;
 
-        xs[graphtime++] = timemillisec;
+        xs[graphtime] = timemillisec - timeoffset;
         ys[i] = DACLookup_FullSine_8Bit[i];
+        graphtime++;
 
         // timemillisec = (tv_diff->tv_usec / 1000);
         // printf("time sec : %d\t time usec  : %d\n", tv_diff->tv_sec, tv_diff->tv_usec);
@@ -80,9 +83,8 @@ int main()
     {
         gettimeofday(tv_diff, NULL);
         timemillisec = ((tv_diff->tv_sec - tv_started->tv_sec) * 1000) + (tv_diff->tv_usec / 1000);
-        //timemillisec -= timeoffset;
 
-        xs[graphtime] = timemillisec;
+        xs[graphtime] = timemillisec - timeoffset;
         ys[graphtime] = DACLookup_FullSine_8Bit[i];
         graphtime++;
 
