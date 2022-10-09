4141b427569010c0e3ee0b538cdd73e1842abde8
Thomas Herbin <thomas@herbin.xyz>
2022-10-10 01:04:23 +0200

    switch to micro sseconds

--- time.c
+++ time.c
@@ -1,9 +1,11 @@
+
 #include <stdio.h>
 #include <sys/time.h>
 #include <unistd.h>
 #include "pbPlots.h"
 #include "supportLib.h"
 #include "time.h"
+
 
 int draw_image(double *xs, double *ys, int size)
 {
@@ -38,20 +40,23 @@ int main()
 {
     struct timeval *tv_diff;
     struct timeval *tv_started;
-    unsigned long timemillisec;
+    unsigned long etime_usec;
     const int arraysize = 512;
-    // double xs[] = {-2, -1, 0, 1, 2};
-    // double ys[] = {2, -1, -2, -1, 2};
-    double *xs;
-    double *ys;
+    double xs[512];
+    double ys[512];
+
+    // double *xs;
+    // double *ys;
+    // xs = malloc(sizeof(double) * arraysize);
+    // ys = malloc(sizeof(double) * arraysize);
 
     int timeoffset;
+    useconds_t usleeptime = 500;
 
     tv_diff = malloc(sizeof(*tv_diff));
     tv_started = malloc(sizeof(*tv_started));
-    xs = malloc(sizeof(double) * arraysize);
-    ys = malloc(sizeof(double) * arraysize);
 
+    
     gettimeofday(tv_started, NULL);
 
     int graphtime = 0;
@@ -60,36 +65,36 @@ int main()
     {
         gettimeofday(tv_diff, NULL);
 
-        timemillisec = ((tv_diff->tv_sec - tv_started->tv_sec) * 1000) + (tv_diff->tv_usec / 1000);
+        etime_usec = ((tv_diff->tv_sec - tv_started->tv_sec) * 1000000) + tv_diff->tv_usec;
         // Get the real starting value so that the graph starts at 0
         if (i == 0)
-            timeoffset = timemillisec;
+            timeoffset = etime_usec;
 
-        xs[graphtime] = timemillisec - timeoffset;
+        xs[graphtime] = etime_usec - timeoffset;
         ys[i] = DACLookup_FullSine_8Bit[i];
         graphtime++;
 
-        // timemillisec = (tv_diff->tv_usec / 1000);
+        // etime_usec = (tv_diff->tv_usec / 1000);
         // printf("time sec : %d\t time usec  : %d\n", tv_diff->tv_sec, tv_diff->tv_usec);
-        // printf("time milli : %lu \t time usec  : %lu\n", timemillisec, tv_diff->tv_usec);
-        // printf("time milli : %lu \t value  : %d\n", timemillisec, DACLookup_FullSine_8Bit[i]);
-        // printf("time sec : %lu \n", timemillisec);
+        //printf("time milli : %lu \t time usec  : %lu\n", etime_usec - timeoffset, tv_diff->tv_usec);
+        // printf("time milli : %lu \t value  : %d\n", etime_usec, DACLookup_FullSine_8Bit[i]);
+        // printf("time sec : %lu \n", etime_usec);
 
         // 1ms
-        usleep(1000);
+        usleep(usleeptime);
     }
 
     for (int i = 0; i < 256; i++)
     {
         gettimeofday(tv_diff, NULL);
-        timemillisec = ((tv_diff->tv_sec - tv_started->tv_sec) * 1000) + (tv_diff->tv_usec / 1000);
+        etime_usec = ((tv_diff->tv_sec - tv_started->tv_sec) * 1000000) + tv_diff->tv_usec;
 
-        xs[graphtime] = timemillisec - timeoffset;
+        xs[graphtime] = etime_usec - timeoffset;
         ys[graphtime] = DACLookup_FullSine_8Bit[i];
         graphtime++;
 
         // 1ms
-        usleep(1000);
+        usleep(usleeptime);
     }
 
     draw_image(xs, ys, arraysize);
