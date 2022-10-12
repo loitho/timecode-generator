ed9af953a5c100f2a8df91b19c27f7d610d73c2a
Thomas HERBIN <thomas@herbin.xyz>
2022-10-12 03:35:42 +0200

    working low - high

--- mytime.c
+++ mytime.c
@@ -7,7 +7,19 @@
 #include "supportLib.h"
 #include "mytime.h"
 
-#define NANOSECONDS_PER_SECOND   1000000000
+#define NANOSECONDS_PER_SECOND 1000000000
+#define DIV_TO_GRAPH_MS 1000
+#define DIV_TO_GRAPH_S 1000
+
+// MARK  => 3Vpp
+// SPACE => 1Vpp
+
+#define SIGNAL_SPACE 0.3
+#define OFFSET_SPACE (2048 - (2048 * SIGNAL_SPACE))
+
+#define SIGNAL_MARK 1
+#define OFFSET_MARK 0
+
 
 int draw_image(double *xs, double *ys, int size)
 {
@@ -38,129 +50,120 @@ int draw_image(double *xs, double *ys, int size)
     return success ? 0 : 1;
 }
 
+
+void send_signal(int signal_type, int offset)
+{
+
+}
+
+
 int main()
 {
-    struct timeval *tv_diff;
-    struct timeval *tv_started;
+    struct timespec *tv_diff;
+    struct timespec *tv_started;
 
     struct timespec *tv_sleep;
-    
-    uint64_t test;
 
-    
-    unsigned long etime_usec;
+    // Elapsed Time
+    uint64_t etime_nsec;
     const int arraysize = 512;
     double xs[512];
     double ys[512];
 
-
-  struct timespec tps, tpe;
-  if ((clock_gettime(CLOCK_REALTIME, &tps) != 0)
-  || (clock_gettime(CLOCK_REALTIME, &tpe) != 0)) {
-    perror("clock_gettime");
-    return -1;
-  }
-  printf("%lu s, %lu ns\n", tpe.tv_sec-tps.tv_sec,
-    tpe.tv_nsec-tps.tv_nsec);
-
+    struct timespec tps, tpe;
+    if ((clock_gettime(CLOCK_REALTIME, &tps) != 0) || (clock_gettime(CLOCK_REALTIME, &tpe) != 0))
+    {
+        perror("clock_gettime");
+        return -1;
+    }
+    printf("%lu s, %lu ns\n", tpe.tv_sec - tps.tv_sec,
+           tpe.tv_nsec - tps.tv_nsec);
 
     // double *xs;
     // double *ys;
     // xs = malloc(sizeof(double) * arraysize);
     // ys = malloc(sizeof(double) * arraysize);
 
-    int timeoffset;
-
-    // Ideal Sleep time is : 1/256 ms = 3.90625 microseconds
-    // Because : you need 256 different values (1 Period in 1 Millisecond)
-    int usleeptime = 0;
-    // int usleeptime = 1000;
+    uint64_t timeoffset;
 
     tv_diff = malloc(sizeof(*tv_diff));
     tv_started = malloc(sizeof(*tv_started));
-    tv_sleep = malloc(sizeof(time_t) + sizeof(long));
 
-    tv_sleep->tv_sec = 0;
-    tv_sleep->tv_nsec = 4000;
 
-    // clock_nanosleep(CLOCK_MONOTONIC, 0, tv_sleep, NULL);
-    // nanosleep(tv_sleep, NULL);
-
-    gettimeofday(tv_started, NULL);
+    clock_gettime(CLOCK_REALTIME, tv_started);
 
     int graphtime = 0;
 
-    int sleepcounter = 0;
-    int starttime = 0;
-    // Microseconds
-    int sleepcounter_period = 4;
+    uint64_t starttime = 0;
+
+    uint64_t sleepcounter = 0;
+    // Ideal Sleep time is : 1/256 ms = 3.90625 microseconds = 3906.25 nanoseconds
+    // Because : you need 256 different values (1 Period in 1 Millisecond)
+    // A bit lower to prevent issues
+    uint64_t sleep_period_nsec = 3800;
 
     for (int i = 0; i < 256; i++)
     {
-        gettimeofday(tv_diff, NULL);
-
-        etime_usec = ((tv_diff->tv_sec - tv_started->tv_sec) * 1000000) + tv_diff->tv_usec;
+        // gettimeofday(tv_diff, NULL);
+        clock_gettime(CLOCK_REALTIME, tv_diff);
+        etime_nsec = ((tv_diff->tv_sec - tv_started->tv_sec) * NANOSECONDS_PER_SECOND) + tv_diff->tv_nsec;
         // Get the real starting value so that the graph starts at 0
         if (i == 0)
-            timeoffset = etime_usec;
+            timeoffset = etime_nsec;
 
-        xs[graphtime] = etime_usec - timeoffset;
-        ys[i] = DACLookup_FullSine_8Bit[i];
+        // +1 to prevent divide by 0
+        xs[graphtime] = (etime_nsec - timeoffset + 1) / DIV_TO_GRAPH_MS;
+        ys[graphtime] = DACLookup_FullSine_8Bit[i] * SIGNAL_LOW + OFFSET_LOW;
         graphtime++;
 
-        // etime_usec = (tv_diff->tv_usec / 1000);
-        // printf("time sec : %d\t time usec  : %d\n", tv_diff->tv_sec, tv_diff->tv_usec);
-        // printf("time milli : %lu \t time usec  : %lu\n", etime_usec - timeoffset, tv_diff->tv_usec);
-        // printf("time milli : %lu \t value  : %d\n", etime_usec, DACLookup_FullSine_8Bit[i]);
-        // printf("time sec : %lu \n", etime_usec);
-        // printf("hi\n");
-
-        // Small value as we get the start point
-        starttime = etime_usec - timeoffset;
         // Try for active wait
-        int looptime = 0;
-        while (sleepcounter < sleepcounter_period)
+        // int looptime = 0;
+        // Small value as we get the start point
+        starttime = etime_nsec - timeoffset;
+
+        while (sleepcounter < sleep_period_nsec)
         {
-            gettimeofday(tv_diff, NULL);
-            etime_usec = ((tv_diff->tv_sec - tv_started->tv_sec) * 1000000) + tv_diff->tv_usec;
-            // printf("sleepcounter %ld = etime_usec %ld - timeoffset %ld- starttime %ld\n", sleepcounter, etime_usec, timeoffset, starttime);
-            sleepcounter = etime_usec - timeoffset - starttime;
+            // gettimeofday(tv_diff, NULL);
+            clock_gettime(CLOCK_REALTIME, tv_diff);
+            etime_nsec = ((tv_diff->tv_sec - tv_started->tv_sec) * NANOSECONDS_PER_SECOND) + tv_diff->tv_nsec;
+            // printf("sleepcounter %ld = etime_nsec %ld - timeoffset %ld- starttime %ld\n", sleepcounter, etime_nsec, timeoffset, starttime);
+            sleepcounter = etime_nsec - timeoffset - starttime;
             // printf("sleepcounter %ld\n", sleepcounter);
-            looptime++;
+            // looptime++;
         }
-        // printf("hi2\n");
-        //printf("looptime %ld\n", looptime);
+
         sleepcounter = 0;
-        // clock_nanosleep(CLOCK_MONOTONIC, 0, tv_sleep, NULL);
-        //  nanosleep(tv_sleep, NULL);
-        //   usleep(usleeptime);
     }
 
     sleepcounter = 0;
+
     for (int i = 0; i < 256; i++)
     {
-        gettimeofday(tv_diff, NULL);
-        etime_usec = ((tv_diff->tv_sec - tv_started->tv_sec) * 1000000) + tv_diff->tv_usec;
+        // gettimeofday(tv_diff, NULL);
+        clock_gettime(CLOCK_REALTIME, tv_diff);
+        etime_nsec = ((tv_diff->tv_sec - tv_started->tv_sec) * NANOSECONDS_PER_SECOND) + tv_diff->tv_nsec;
 
-        xs[graphtime] = etime_usec - timeoffset;
+        xs[graphtime] = (etime_nsec - timeoffset + 1) / DIV_TO_GRAPH_MS;
         ys[graphtime] = DACLookup_FullSine_8Bit[i];
         graphtime++;
 
         // Small value as we get the start point
-        starttime = etime_usec - timeoffset;
+        starttime = etime_nsec - timeoffset;
         // Try for active wait
         int looptime = 0;
-        while (sleepcounter < sleepcounter_period)
+        while (sleepcounter < sleep_period_nsec)
         {
-            gettimeofday(tv_diff, NULL);
-            etime_usec = ((tv_diff->tv_sec - tv_started->tv_sec) * 1000000) + tv_diff->tv_usec;
-            // printf("sleepcounter %ld = etime_usec %ld - timeoffset %ld- starttime %ld\n", sleepcounter, etime_usec, timeoffset, starttime);
-            sleepcounter = etime_usec - timeoffset - starttime;
+            // gettimeofday(tv_diff, NULL);
+            clock_gettime(CLOCK_REALTIME, tv_diff);
+
+            etime_nsec = ((tv_diff->tv_sec - tv_started->tv_sec) * NANOSECONDS_PER_SECOND) + tv_diff->tv_nsec;
+            // printf("sleepcounter %ld = etime_nsec %ld - timeoffset %ld- starttime %ld\n", sleepcounter, etime_nsec, timeoffset, starttime);
+            sleepcounter = etime_nsec - timeoffset - starttime;
             // printf("sleepcounter %ld\n", sleepcounter);
             looptime++;
         }
         // printf("hi2\n");
-        //printf("looptime %ld\n", looptime);
+        // printf("looptime %ld\n", looptime);
         sleepcounter = 0;
         // clock_nanosleep(CLOCK_MONOTONIC, 0, tv_sleep, NULL);
         //  nanosleep(tv_sleep, NULL);
