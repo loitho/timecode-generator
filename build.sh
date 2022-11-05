5ba24bed03e6aa4fa9e7094f43a848fdda12ea6b
Thomas HERBIN <thomas@herbin.xyz>
2022-11-05 02:26:47 +0100

    update build

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-/usr/bin/gcc -fdiagnostics-color=always -O3 \
+/usr/bin/gcc -pthread -fdiagnostics-color=always -O3 \
         -Wall -Wextra -L./include \
         -lpbplots -Wl,-rpath=./include \
-        -I./include ./mytime.c \
-        -o ./mytime -lpbplots -lm -lpbplots -lwiringPi
+        -I./include ./*.c \
+        -o ./mytime -lpbplots -lm -lpbplots -lrt
 
 # sh build.sh && sudo nice  --adjustment=-20 ./mytime && python -m http.server 8080
